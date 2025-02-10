57e1f11ca8442bbd4c3d99c81fb18e069a63dc52
LoickBck <loickbuck@hotmail.com>
2025-02-10 20:27:00 +0100

    init

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import { DarkModeToggle } from '@/components/DarkModeToggle'
+
+function Header() {
+    return (
+        <div>
+            <DarkModeToggle />
+        </div>
+    )
+}
+export default Header
